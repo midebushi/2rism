bd3128709cdf172d831fac587a29f20b35ff5844
midebushi <dgibadulllin@mail.ru>
2026-01-17 13:38:40 +0500

    Fix: config

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/2rism/',
-  server: {
-    host: '0.0.0.0',
-    port: 5173, 
-    strictPort: true, 
-  }
 })
